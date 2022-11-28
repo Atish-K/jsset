c9640f6d24a585a3b8cadf2c5a24240b2a5f043e
Atish-K <atish0518@gmail.com>
2022-11-28 21:30:02 +0530

    duparray

--- duplicateArray.js
+++ duplicateArray.js
@@ -1,20 +1,20 @@
 
 // Using Filter
-let language = ['JavaScript', 'Dart', 'Kotlin', 'Java', 'Swift', 'Dart', 'JavaScript'];
-function unique_array(arr) {
-   let unique_array = arr.filter(function (elem, index, self) {
-       return index == self.indexOf(elem);
-   });
-   return unique_array
-}
-console.log(unique_array(language));
+// let language = ['JavaScript', 'Dart', 'Kotlin', 'Java', 'Swift', 'Dart', 'JavaScript'];
+// function unique_array(arr) {
+//    let unique_array = arr.filter(function (elem, index, self) {
+//        return index == self.indexOf(elem);
+//    });
+//    return unique_array
+// }
+// console.log(unique_array(language));
 
 // using loop
 
 let language = ['JavaScript', 'Dart', 'Kotlin', 'Java', 'Swift', 'Dart', 'JavaScript'];
 function dups_array(language) {
    let unique = {};
-   langugae.forEach(function (i) {
+   language.forEach(function (i) {
        if(!unique[i]) {
           unique[i] = true;
        }
