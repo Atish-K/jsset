c22f318e2aba56669847844de1a55f68653f180c
Atish-K <atish0518@gmail.com>
2022-11-28 21:13:31 +0530

    event capturing

--- eventCapturing.js
+++ eventCapturing.js
@@ -13,20 +13,20 @@ addEvent(document, 'DOMContentLoaded', ()=>{
 
     addEvent(child, 'click', function(e){
         console.log('child');
-    })
+    }, true);
     addEvent(parent, 'click', function(e){
         console.log('parent');
-    })
+    }, true);
     addEvent(grandparent, 'click', function(e){
         console.log('grandparent');
-    })
+    }, true);
     addEvent(document, 'click', function(e){
         console.log('document');
-    })
+    }, true);
     addEvent(html, 'click', function(e){
         console.log('html');
-    })
+    }, true);
     addEvent(window, 'click', function(e){
         console.log('window');
-    })
+    }, true);
 })
