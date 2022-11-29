00f821f5172d30209c5faa6cf55f718d9b555b9e
Atish-K <atish0518@gmail.com>
2022-11-29 23:15:29 +0530

    mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -3,26 +3,18 @@
  {
      var n1 = m - l + 1;
      var n2 = r - m;
-  
-     // Create temp arrays
+
      var L = new Array(n1);
      var R = new Array(n2);
-  
-     // Copy data to temp arrays L[] and R[]
+
      for (var i = 0; i < n1; i++)
          L[i] = arr[l + i];
      for (var j = 0; j < n2; j++)
          R[j] = arr[m + 1 + j];
   
-     // Merge the temp arrays back into arr[l..r]
-  
-     // Initial index of first subarray
      var i = 0;
-  
-     // Initial index of second subarray
+
      var j = 0;
-  
-     // Initial index of merged subarray
      var k = l;
   
      while (i < n1 && j < n2) {
@@ -42,9 +34,6 @@
          i++;
          k++;
      }
-  
-     // Copy the remaining elements of
-     // R[], if there are any
      while (j < n2) {
          arr[k] = R[j];
          j++;
